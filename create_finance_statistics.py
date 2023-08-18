5bfc1930a185d1a0a350fc9872ab9893ea45259c
whbarndt <whbarndt@protonmail.com>
2023-08-17 20:59:21 -0800

    added stats visualization with seaborn

--- create_finance_statistics.py
+++ create_finance_statistics.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 import datetime as dt
 import sys
 import yaml
@@ -26,19 +28,50 @@ businesses = expense_df['Business'].unique()
 unique_grouped_tags = expense_df['Tags'].unique()
 all_tags = '/'.join(expense_df['Tags']).split('/')
 unique_individual_tags = set(all_tags)
+tags_for_stats = ['Groceries', 'Snacks', 'Meal', 'Coffee', 'Entertainment', 'Membership']
 
 # Create Statistics Dataframe
 stats_df = pd.DataFrame()
 
+# Statistics Column Headers
+column_headers = ['Month', 'Total_Expenses']
+for tag in tags_for_stats:
+    column_headers.append(f'{tag}_Expenses')
+
 # Grab months from expense report
-column_headers = ['Month', 'Total_Expenses', 'Grocery_Expenses', 'Snack_Expenses', 'Coffee_Expenses']
-months_in_expenses = expense_df.groupby(expense_df['Date'].dt.to_period('M'))
-for month_num in months_in_expenses:
-    row_stats = []
-    row_stats.append(month_num)
+months_in_expenses = expense_df['Date'].dt.strftime("%m").unique().tolist()
+
+# Create stat rows 
+for month_num_str in months_in_expenses:
+    row_stats = {}
+    month_num = int(month_num_str)
+    row_stats['Month'] = month_num
     month_expenses = expense_df[expense_df['Date'].dt.month == month_num]
-    row_stats.append(month_expenses['Cost'].sum())
-    '''for tag in tags:
+    row_stats['Total_Expenses'] = month_expenses['Cost'].sum()
+    for tag in tags_for_stats:
         tag_month_expense = month_expenses[month_expenses['Tags'].str.contains(tag, case=False, na=False)]
-        tag_cost = tag_month_expense['Cost'].sum()
-        stats_df[f'{tag}_Expenses'] = tag_cost'''
+        row_stats[f'{tag}_Expenses'] = tag_month_expense['Cost'].sum()
+    row_stats_df = pd.DataFrame([row_stats])
+    stats_df = pd.concat([stats_df, row_stats_df])
+stats_df.set_index('Month', inplace=True)
+print(stats_df)
+
+# Stats Visualization
+sns.set_theme()
+
+plt.figure(figsize=[12, 8])
+ax = sns.lineplot(data=stats_df.drop(columns=['Total_Expenses']), markers=True)
+
+plt.title(f"{year} Expenses by Month")
+plt.xlabel("Months")
+plt.ylabel("Expense Cost ($)")
+
+plt.savefig(f"{year}_Tag_Expenses.png")
+
+'''plt, ax = plt.subplots(figsize=[12,8])
+ax.set_title(f"{year} Expenses by Month")
+ax.set_xlabel("Months")
+ax.set_ylabel("Expense Cost ($)")
+stats_df_no_total = stats_df.drop(columns=['Total_Expenses'])
+stats_df_no_total.plot(ax=ax)
+plt.savefig(f"{year}_Tag_Expenses.png")'''
