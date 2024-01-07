e72928360f57845f05422041c758a940b05132c8
Hunter <whbarndt@protonmail.com>
2024-01-06 21:27:36 -0900

    fixed update_yaml_files.py and config use

--- update_yaml_files.py
+++ update_yaml_files.py
@@ -3,20 +3,29 @@ import pandas as pd
 import datetime as dt
 import sys
 import yaml
+import os
 
 # Specify the path to the YAML file
-config_yaml_file_path = 'config.finance.yml'
-businesses_yaml_file_path = 'businesses.finance.yml'
+local_config_yaml_file_path = 'config.local.finance.yml'
 
 # Open and read the YAML files
+with open(local_config_yaml_file_path, 'r') as file:
+    local_config_yaml = yaml.safe_load(file)
+
+config_yaml_file_path = local_config_yaml['Working_Dir'] + 'config.finance.yml'
+
 with open(config_yaml_file_path, 'r') as file:
     config_yaml = yaml.safe_load(file)
+
+businesses_yaml_file_string = 'businesses.finance.yml'
+businesses_yaml_file_path = local_config_yaml['Working_Dir'] + businesses_yaml_file_string
+
 with open(businesses_yaml_file_path, 'r') as file:
     businesses_yaml = yaml.safe_load(file)
 
 # Load data from config file
-year = config_yaml['Year']
-path_to_expense_dir = config_yaml['Expenses_path']
+year = local_config_yaml['Year']
+path_to_expense_dir = local_config_yaml['Working_Dir']
 
 # Read Expense File
 expense_file = f'{year}-Expense-Report.csv'
@@ -31,28 +40,29 @@ config_tags = config_yaml['Tags']
 report_locations = expense_df['Location'].unique()
 report_businesses = expense_df['Business'].unique()
 report_unique_tags = expense_df['Tags'].unique()
-report_all_tags = '/'.join(list(report_unique_tags)).split('/')
-report_unique_individual_tags = list(set(report_all_tags))
+#report_all_tags = '/'.join(list(report_unique_tags)).split('/')
+#report_unique_individual_tags = list(set(report_all_tags))
 
 # Append both lists and get unique values from it
 config_locations.extend(report_locations)
 config_businesses.extend(report_businesses)
-config_tags.extend(report_unique_individual_tags)
+config_tags.extend(report_unique_tags)
 all_unique_locations = list(set(config_locations))
 all_unique_businesses = list(set(config_businesses))
-all_unique_individual_tags = list(set(config_tags))
+all_unique_tags = list(set(config_tags))
 
 # Sort and Replace config data with updated data
 np.sort(all_unique_locations)
 np.sort(all_unique_businesses)
-np.sort(all_unique_individual_tags)
+np.sort(all_unique_tags)
 config_yaml['Locations'] = all_unique_locations
-config_yaml['Tags'] = all_unique_individual_tags
 config_yaml['Businesses'] = all_unique_businesses
+config_yaml['Tags'] = all_unique_tags
+
 
 # Get all optional locations and tags for businesses from expense report and replace existing ones
 all_buissnesses = {}
-for buisness in all_unique_businesses:
+for buisness in report_businesses:
     all_business_tags = []
     all_business_locs = []
     expense_business_rows = expense_df[expense_df['Business'] == buisness]
